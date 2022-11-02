39f52cf9416f7ec04f38efab029858e86a9a470d
Ian Applegate <ian@jedi.sh>
2022-11-02 17:13:01 -0500

    examples: fix command buffer coercion, update mach to latest

--- examples/basic/main.zig
+++ examples/basic/main.zig
@@ -111,7 +111,7 @@ pub fn update(app: *App, core: *mach.Core) !void {
     var command = encoder.finish(null);
     encoder.release();
 
-    app.queue.submit(&.{command});
+    app.queue.submit(&[_]*gpu.CommandBuffer{command});
     command.release();
 
     core.swap_chain.?.present();
