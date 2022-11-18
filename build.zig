ecb416a33dfce0e0096e8485f09a07d28149d1f6
kdchambers <kd.chambers97@gmail.com>
2022-11-18 05:06:03 -0500

    all: Remove example code + mach comptime dependency
    Mach dependency is satisfied using comptime dependency injection

--- build.zig
+++ build.zig
@@ -1,10 +1,16 @@
 const std = @import("std");
 const Pkg = std.build.Pkg;
 
+const this_dir = thisDir();
+
+inline fn thisDir() []const u8 {
+    return comptime std.fs.path.dirname(@src().file) orelse ".";
+}
+
 pub fn getPkg(dependencies: []const Pkg) Pkg {
     return .{
         .name = "mach-imgui",
-        .source = .{ .path = thisDir() ++ "/src/main.zig" },
+        .source = .{ .path = this_dir ++ "/src/main.zig" },
         .dependencies = dependencies,
     };
 }
@@ -12,60 +18,33 @@ pub fn getPkg(dependencies: []const Pkg) Pkg {
 pub fn link(exe: *std.build.LibExeObjStep) void {
     exe.defineCMacro("IMGUI_IMPL_API", "extern \"C\"");
 
-    exe.addIncludePath(thisDir() ++ "/src");
-    exe.addIncludePath(thisDir() ++ "/libs");
-    exe.addIncludePath(thisDir() ++ "/libs/imgui");
+    exe.addIncludePath(this_dir ++ "/src");
+    exe.addIncludePath(this_dir ++ "/libs");
+    exe.addIncludePath(this_dir ++ "/libs/imgui");
 
-    exe.addCSourceFile(thisDir() ++ "/src/zgui.cpp", &.{""});
-    exe.addCSourceFile(thisDir() ++ "/libs/imgui/imgui.cpp", &.{""});
-    exe.addCSourceFile(thisDir() ++ "/libs/imgui/imgui_widgets.cpp", &.{""});
-    exe.addCSourceFile(thisDir() ++ "/libs/imgui/imgui_tables.cpp", &.{""});
-    exe.addCSourceFile(thisDir() ++ "/libs/imgui/imgui_draw.cpp", &.{""});
-    exe.addCSourceFile(thisDir() ++ "/libs/imgui/imgui_demo.cpp", &.{""});
-    exe.addCSourceFile(thisDir() ++ "/libs/imgui/imgui_impl_wgpu.cpp", &.{""});
-    exe.addCSourceFile(thisDir() ++ "/libs/imgui/imgui_impl_mach.cpp", &.{""});
-    exe.addCSourceFile(thisDir() ++ "/libs/imgui/implot_demo.cpp", &.{""});
-    exe.addCSourceFile(thisDir() ++ "/libs/imgui/implot.cpp", &.{""});
-    exe.addCSourceFile(thisDir() ++ "/libs/imgui/implot_items.cpp", &.{""});
-}
+    exe.addCSourceFile(this_dir ++ "/src/zgui.cpp", &.{""});
+    exe.addCSourceFile(this_dir ++ "/libs/imgui/imgui.cpp", &.{""});
+    exe.addCSourceFile(this_dir ++ "/libs/imgui/imgui_widgets.cpp", &.{""});
+    exe.addCSourceFile(this_dir ++ "/libs/imgui/imgui_tables.cpp", &.{""});
+    exe.addCSourceFile(this_dir ++ "/libs/imgui/imgui_draw.cpp", &.{""});
+    exe.addCSourceFile(this_dir ++ "/libs/imgui/imgui_demo.cpp", &.{""});
+    exe.addCSourceFile(this_dir ++ "/libs/imgui/imgui_impl_wgpu.cpp", &.{""});
+    exe.addCSourceFile(this_dir ++ "/libs/imgui/imgui_impl_mach.cpp", &.{""});
+    exe.addCSourceFile(this_dir ++ "/libs/imgui/implot_demo.cpp", &.{""});
+    exe.addCSourceFile(this_dir ++ "/libs/imgui/implot.cpp", &.{""});
+    exe.addCSourceFile(this_dir ++ "/libs/imgui/implot_items.cpp", &.{""});
 
-inline fn thisDir() []const u8 {
-    return comptime std.fs.path.dirname(@src().file) orelse ".";
+    exe.linkLibraryCpp();
 }
 
 pub fn build(b: *std.build.Builder) !void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
-    const name = "example-basic";
-    const mach = @import("examples/libs/mach/build.zig");
+    const tests = b.addTest(this_dir ++ "/src/main.zig");
+    tests.setBuildMode(mode);
+    tests.setTarget(target);
 
-    const mach_imgui_pkg = getPkg(&[_]Pkg{mach.pkg});
-
-    const example_app = try mach.App.init(
-        b,
-        .{
-            .name = name,
-            .src = "./examples/basic/main.zig",
-            .target = target,
-            .deps = &[_]Pkg{mach_imgui_pkg},
-        },
-    );
-
-    example_app.setBuildMode(mode);
-    try example_app.link(.{});
-    link(example_app.step);
-
-    const compile_step = b.step("compile-" ++ name, "Compile " ++ name);
-    compile_step.dependOn(&b.addInstallArtifact(example_app.step).step);
-    b.getInstallStep().dependOn(compile_step);
-
-    const run_cmd = try example_app.run();
-    run_cmd.dependOn(compile_step);
-
-    const test_step = b.step("test", "Test");
-    test_step.dependOn(compile_step);
-
-    const run_step = b.step("run-" ++ name, "Run " ++ name);
-    run_step.dependOn(run_cmd);
+    const test_step = b.step("test", "Run mach-imgui tests");
+    test_step.dependOn(&tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,2 +1,6 @@
-pub usingnamespace @import("zgui.zig");
-pub const backend = @import("zgui_mach.zig");
+pub fn MachImgui(comptime mach: anytype) type {
+    return struct {
+        pub usingnamespace @import("zgui.zig");
+        pub const backend = @import("zgui_mach.zig").MachBackend(mach);
+    };
+}
